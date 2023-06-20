8c42a83ffd76d329e148674819213eea38f2917e
Виталий Моисеенко <88559269+QFolli@users.noreply.github.com>
2023-06-20 10:42:27 +0300

    Add files via upload

---
+++ task78/task78_2/task78_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+// Функция, которая будет выполняться в потоке
+void calculateSquare(int number) {
+    int square = number * number;
+    cout << "Квадрат числа " << number << ": " << square << endl;
+}
+
+int main() {
+    int number;
+
+    cout << "Введите целое число: ";
+    cin >> number;
+
+    // Создание потока и передача ему функции calculateSquare
+    thread thread(calculateSquare, number);
+
+    // Ожидание завершения работы потока
+    thread.join();
+
+    return 0;
+}
